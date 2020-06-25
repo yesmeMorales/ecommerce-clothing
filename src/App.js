1abaa6afdba22024adc2de444bb6c46d2cf99ccd
Yesme <moralesyesme@gmail.com>
2020-06-24 23:04:50 -0500

    Adding custom-button and form-input with its styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component.jsx";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route path="/Shop" component={ShopPage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
+        <Route path="/signin" component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
