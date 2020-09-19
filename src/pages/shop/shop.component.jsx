481d933d87d6e465bd4414fd03aa0189dd5bacd1
Yesme <moralesyesme@gmail.com>
2020-09-19 18:33:08 -0500

    data normalization addedand memoization in collection

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route } from "react-router-dom";
 // import { connect } from "react-redux";
 // import SHOP_DATA from "./shop.data";
 // import { createStructuredSelector } from "reselect";
@@ -6,11 +7,18 @@ import React from "react";
 // import CollectionPreview from "../../components/collection-preview/collection-preview";
 // import { selectCollections } from "../../redux/shop/shop.selectors";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = () => (
-  <div className="shop-page">
-    <CollectionsOverview />
-  </div>
-);
+const ShopPage = ({ match }) => {
+  console.log("path" + match.path);
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      {/* collectionId have to match with
+      collection.component(ownProps.match.params.collectionId) */}
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
